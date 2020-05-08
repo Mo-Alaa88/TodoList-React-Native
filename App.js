c7142767ab6065d896d408c3ea1d7afbf964220e
mo-alaa88 <mohammed.alaa.al.din@gmail.com>
2020-05-08 17:46:15 +0200

    Create Todo React-Native We can add new task

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './cmponents/header.js';
+import TodoItem from './cmponents/todoItem.js';
+import AddTodo from './cmponents/addTodo.js';
+
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text:'buy coffe', key:'1'},
+    {text:'create an App', key:'2'},
+    {text:'play on the switch', key:'3'},
+  ]);
+  const pressHandler = (key) => {
+    setTodos((preveTodos)=>{
+      return preveTodos.filter(todos=> todos.key != key);
+    })
+  };
+
+  const submitHandler=(text)=>{
+    setTodos((preveTodos)=>(
+      [
+        {text:text , key:Math.random().toString()},
+        ...preveTodos
+      ]
+    )
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* to header */}
+      <Header/>
+      <View style={styles.content}>
+        {/* to form */}
+        <AddTodo submitHandler={submitHandler}/>
+        {/* to todo */}
+        <View style={styles.list}>
+          <FlatList
+          // numColumns={2}
+          data={todos}
+          renderItem={({item})=>(
+            <TodoItem item={item} pressHandler={pressHandler}/>
+          )}
+          />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -16,4 +56,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content:{
+    padding:40,
+  },
+  list:{
+    margin:20
+  }
 });
